19aae100c7e23528b27fa12b4316e8c9c84acc8f
maiken-veedla <maiken.veedla@gmail.com>
2021-10-01 19:11:53 +0300

    Added constructor, getters, setters to objects; enum as a separate class

--- src/main/java/dataObjects/Data.java
+++ src/main/java/dataObjects/Data.java
@@ -3,11 +3,42 @@ package dataObjects;
 import java.util.List;
 
 public class Data {
-    enum labelType {
-        TEXT,
-        LINE,
-        DOT,
+    private Type labelType;
+    private int eventSpace; // space between two events
+    private List<Event> events;
+
+    public Data(Type labelType, int eventSpace, List<Event> events) {
+        this.labelType = labelType;
+        this.eventSpace = eventSpace;
+        this.events = events;
     }
-    int eventSpace; // space between two events
-    List<Event> events;
+
+    public Type getLabelType() {
+        return labelType;
+    }
+
+    public void setLabelType(Type labelType) {
+        this.labelType = labelType;
+    }
+
+    public int getEventSpace() {
+        return eventSpace;
+    }
+
+    public void setEventSpace(int eventSpace) {
+        this.eventSpace = eventSpace;
+    }
+
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<Event> events) {
+        this.events = events;
+    }
+
+    //Add a single event to events list
+    public void addEvent(int index, Event event){
+        events.add(index, event);
+    }
 }
--- src/main/java/dataObjects/Event.java
+++ src/main/java/dataObjects/Event.java
@@ -1,11 +1,77 @@
 package dataObjects;
 
 public class Event {
-    Boolean hidden;
-    int queueNr;
-    String label;
-    String htmlContent;
-    int maxHeight;
-    int width;
-    Boolean placedLeft;
+    private Boolean hidden;
+    private int queueNr;
+    private String label;
+    private String htmlContent;
+    private int maxHeight;
+    private int width;
+    private Boolean placedLeft;
+
+    public Event(Boolean hidden, int queueNr, String label, String htmlContent, int maxHeight, int width, Boolean placedLeft) {
+        this.hidden = hidden;
+        this.queueNr = queueNr;
+        this.label = label;
+        this.htmlContent = htmlContent;
+        this.maxHeight = maxHeight;
+        this.width = width;
+        this.placedLeft = placedLeft;
+    }
+
+    public Boolean getHidden() {
+        return hidden;
+    }
+
+    public void setHidden(Boolean hidden) {
+        this.hidden = hidden;
+    }
+
+    public int getQueueNr() {
+        return queueNr;
+    }
+
+    public void setQueueNr(int queueNr) {
+        this.queueNr = queueNr;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getHtmlContent() {
+        return htmlContent;
+    }
+
+    public void setHtmlContent(String htmlContent) {
+        this.htmlContent = htmlContent;
+    }
+
+    public int getMaxHeight() {
+        return maxHeight;
+    }
+
+    public void setMaxHeight(int maxHeight) {
+        this.maxHeight = maxHeight;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public Boolean getPlacedLeft() {
+        return placedLeft;
+    }
+
+    public void setPlacedLeft(Boolean placedLeft) {
+        this.placedLeft = placedLeft;
+    }
 }
