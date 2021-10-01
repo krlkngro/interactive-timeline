94824d52f25ca2ddd5ce314c4e033e1af362793e
maiken-veedla <maiken.veedla@gmail.com>
2021-10-01 17:35:30 +0300

    Data objects #55

---
+++ src/main/java/dataObjects/Event.java
@@ -0,0 +1,11 @@
+package dataObjects;
+
+public class Event {
+    Boolean hidden;
+    int queueNr;
+    String label;
+    String htmlContent;
+    int maxHeight;
+    int width;
+    Boolean placedLeft;
+}
