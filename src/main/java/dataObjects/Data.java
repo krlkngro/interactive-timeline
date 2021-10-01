ffedc436d968691274b2fbe6cf5a2c3e3b8ff652
maiken-veedla <maiken.veedla@gmail.com>
2021-10-01 15:57:28 +0300

    Data objects

---
+++ src/main/java/dataObjects/Data.java
@@ -0,0 +1,10 @@
+package dataObjects;
+
+import java.util.List;
+
+public class Data {
+
+    String labelType; // ["text", "line", "dot"...]
+    int eventSpace; // space between two events
+    List<Event> events;
+}
---
+++ src/main/java/dataObjects/Event.java
@@ -0,0 +1,11 @@
+package dataObjects;
+
+public class Event {
+    Boolean hidden;
+    int orderNr;
+    String label;
+    String htmlContent;
+    int maxHeight;
+    int width;
+    Boolean placedLeft;
+}
